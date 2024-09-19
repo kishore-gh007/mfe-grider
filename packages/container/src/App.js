29db3d9f881813690f035f33fc13da9f8f04f7c3
kishore kumar <kishore.jsx@gmail.com>
2024-09-19 11:35:38 +0530

    updated prod classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./container/MarketingApp";
 import Header from "./container/Header";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
+
   return (
     <BrowserRouter>
-      <h1>Container App - {process.env.NODE_ENV}</h1>
-      <hr />
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <h1>Container App - {process.env.NODE_ENV}</h1>
+        <hr />
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
